47e66f729fbeaf69ad4528ccf2570f57e5ee3701
Isaac Lewis <isaaclewis5001@gmail.com>
2024-01-09 12:54:58 -0700

    Implemented the ChessPosition class.

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -7,23 +7,64 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessPosition {
+    private int row;
+    private int column;
 
     public ChessPosition(int row, int col) {
+        this.row = row;
+        this.column = col;
     }
 
     /**
-     * @return which row this position is in
-     * 1 codes for the bottom row
+     * @return Which row this position is in, with 1
+     * coding for the bottom row.
      */
     public int getRow() {
-        throw new RuntimeException("Not implemented");
+        return row;
     }
 
     /**
-     * @return which column this position is in
-     * 1 codes for the left row
+     * @return Which column this position is in, with 1 coding for the left column.
      */
     public int getColumn() {
-        throw new RuntimeException("Not implemented");
+        return column;
+    }
+
+    /**
+     * @return Whether the position represents a position that is on the chessboard.
+     **/
+    public boolean isOnBoard() {
+        return (
+                row >= 1 &&
+                column >= 1 &&
+                row <= 8 &&
+                column <= 8
+        );
+    }
+
+    /**
+     * Applies a translation to this position.
+     * @param row_off How far to translate forward.
+     * @param col_off How for to translate rightwards.
+     */
+    public void offset(int row_off, int col_off) {
+        row += row_off;
+        column += col_off;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof ChessPosition) {
+            return (
+                    ((ChessPosition)obj).row == row &&
+                    ((ChessPosition)obj).column == column
+            );
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return row << 16 | column;
     }
 }
