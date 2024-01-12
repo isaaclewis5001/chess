0f89cc65cdd10a9e873f5bddf1b61d8a1e2c937a
Isaac Lewis <isaaclewis5001@gmail.com>
2024-01-11 22:27:38 -0700

    Implemented ChessMove.

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -1,38 +1,66 @@
 package chess;
 
 /**
- * Represents moving a chess piece on a chessboard
+ * Represents moving a chess piece on a chessboard.
  * <p>
  * Note: You can add to this class, but you may not alter
  * signature of the existing methods.
  */
 public class ChessMove {
+    ChessPosition startPos;
+    ChessPosition endPos;
+    ChessPiece.PieceType promotion;
+
 
     public ChessMove(ChessPosition startPosition, ChessPosition endPosition,
                      ChessPiece.PieceType promotionPiece) {
+        this.startPos = startPosition;
+        this.endPos = endPosition;
+        this.promotion = promotionPiece;
     }
 
     /**
-     * @return ChessPosition of starting location
+     * @return ChessPosition of starting location.
      */
     public ChessPosition getStartPosition() {
-        throw new RuntimeException("Not implemented");
+        return startPos;
     }
 
     /**
-     * @return ChessPosition of ending location
+     * @return ChessPosition of ending location.
      */
     public ChessPosition getEndPosition() {
-        throw new RuntimeException("Not implemented");
+        return endPos;
     }
 
     /**
      * Gets the type of piece to promote a pawn to if pawn promotion is part of this
-     * chess move
+     * chess move.
      *
-     * @return Type of piece to promote a pawn to, or null if no promotion
+     * @return Type of piece to promote a pawn to, or null if no promotion.
      */
     public ChessPiece.PieceType getPromotionPiece() {
-        throw new RuntimeException("Not implemented");
+        return promotion;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof ChessMove other) {
+            return (
+                    startPos.equals(other.startPos) &&
+                    endPos.equals(other.endPos) &&
+                    promotion == other.promotion
+            );
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        int xor = startPos.hashCode() ^ endPos.hashCode();
+        if (promotion == null) {
+            return xor;
+        }
+        return xor - (promotion.ordinal() + 1) << 8;
     }
 }
--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -47,13 +47,15 @@ public class ChessPiece {
     /**
      * Calculates all the positions a chess piece can move to
      * Does not take into account moves that are illegal due to leaving the king in
-     * danger
+     * danger.
      *
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
         throw new RuntimeException("Not implemented");
     }
+
+
 
     @Override
     public boolean equals(Object obj) {
