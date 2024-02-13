7f785a51dd51846e5740c5267dbd84e536c06bf7
Isaac Lewis <isaaclewis5001@gmail.com>
2024-02-12 19:28:39 -0700

    Started project 3.

---
+++ server/src/main/java/server/Server.java
@@ -0,0 +1,20 @@
+package server;
+
+import spark.*;
+
+public class Server {
+
+    public int run(int desiredPort) {
+        Spark.port(desiredPort);
+
+        Spark.staticFiles.location("web");
+        Spark.init();
+        Spark.awaitInitialization();
+        return Spark.port();
+    }
+
+    public void stop() {
+        Spark.stop();
+        Spark.awaitStop();
+    }
+}
