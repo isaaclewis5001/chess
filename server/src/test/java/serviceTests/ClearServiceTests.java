553251b04b079f45dde9ed5cd2e6ba2e3d87176e
Isaac Lewis <isaaclewis5001@gmail.com>
2024-02-27 22:10:22 -0700

    Added tests for ClearService

--- server/src/test/java/serviceTests/ClearServiceTests.java
+++ server/src/test/java/serviceTests/ClearServiceTests.java
@@ -1,13 +1,49 @@
 package serviceTests;
 
-
+import model.AuthData;
+import model.UserData;
+import model.request.CreateGameRequest;
+import model.request.JoinGameRequest;
+import model.request.LoginRequest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import service.ClearService;
+import server.bundles.DataAccess;
+import server.bundles.MemoryDataAccess;
+import server.bundles.Services;
+import service.GamesService;
+import service.UserService;
 
 
 @DisplayName("Clear Service Tests")
 public class ClearServiceTests {
+    private Services services;
 
+    private void setup() {
+        DataAccess dataAccess = new MemoryDataAccess();
+        services = new Services(dataAccess);
+    }
+
+    @Test
+    @DisplayName("Clear")
+    public void clear() throws Exception {
+        setup();
+
+        AuthData auth = services.userService().createUser(new UserData("a", "b", "c"));
+        int game = services.gamesService().createGame(new CreateGameRequest("game")).gameID();
+
+        services.clearService().clear();
+
+        Assertions.assertThrows(UserService.BadLoginException.class,
+            () -> services.userService().login(new LoginRequest("a", "b"))
+        );
+
+        Assertions.assertThrows(UserService.BadAuthException.class,
+            () -> services.userService().getAuthUser(auth.authToken())
+        );
+
+        Assertions.assertThrows(GamesService.BadGameIdException.class,
+            () -> services.gamesService().joinGame(new JoinGameRequest(game, null), "a")
+        );
+    }
 }
